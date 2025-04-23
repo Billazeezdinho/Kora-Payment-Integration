aed33ae72ef8d8543bc060210ae03d28f25e5fd4
Billazeezdinho <billazeezdinho@gmail.com>
2025-04-23 02:32:41 -0400

    get all transaction

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -1,6 +1,7 @@
 const {transactionModel} = require('../models/transactionModel');
 const axios = require('axios')
 const otpGenerator = require('otp-generator');
+const { message } = require('statuses');
 const otp = otpGenerator.generate(12, {specialChars: false})
 const ref =  `TCA-AF-${otp}`;
 const secret_key = process.env.korapay_secret_key
@@ -23,11 +24,13 @@ exports.initializePayment = async (req, res) =>{
             currency: 'NGN',
             reference: ref
         };
-        const response = await axios.post('https://api.korapay.com/merchant/api/v1/charges/initialize', paymentData, {
+        console.log( 'Before Payment' )
+        const response = await axios.post('https://api.korapay.com/merchant/api/v1/charges/initialize',paymentData, {
             headers: {
                 Authorization: `Bearer ${secret_key}`
             }
         });
+        console.log( 'After Payment' )
         
         const { data } = response?.data;
         const payment = new transactionModel({
@@ -53,5 +56,62 @@ exports.initializePayment = async (req, res) =>{
             message: ' Internal server error'
         })
         
+    }
+}
+
+exports.verifyPayment = async (req, res) => {
+    try {
+        const { reference } = req.query;
+        const response = await axios.get(`https://api.korapay.com/merchant/api/v1/charges/${reference}`,{
+            headers: { Authorization: ` Bearer ${secret_key}`}
+        });
+        const { data } = response?.data;
+
+        if(data?.status && data?.status === 'success' ){
+            const payment = await transactionModel.findOneAndUpdate({reference}, {status: 'success'}, {new:true});
+            res.status(200).json({
+                message: 'Payment Verification Successfully',
+                data: payment
+            })
+        }else{
+            const payment = await transactionModel.findOneAndUpdate({reference}, {status: 'failed'}, {new:true});
+            res.status(200).json({
+                message: 'Payment Verification Failed',
+                data: payment
+            })
+
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            message: 'Internal Server Error' + error.message
+        })
+        
+    }
+}
+
+exports.getAllTransaction = async (req, res) => {
+    try {
+
+        const trans = await transactionModel.find({ })
+        if(!trans){
+            return res.status(404).json({
+                message: 'No Transaction Found'
+            })
+        }
+        if(trans.length <= 0){
+            return res.status(404).json({
+                message: 'No Transaction fOUND'
+            })
+        }
+        res.status(200).josn({
+            message: 'successfully fetch List of Transaction',
+            data: trans
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'internal server error' + error.message
+        })
     }
 }
--- routes/transactionRouter.js
+++ routes/transactionRouter.js
@@ -1,7 +1,9 @@
-const { initializePayment } = require('../controller/transactionController');
+const { initializePayment, verifyPayment } = require('../controller/transactionController');
 
 const router = require('express').Router()
 
 router.post('/initialize', initializePayment)
+
+router.get('/verify', verifyPayment)
 
 module.exports = router;
